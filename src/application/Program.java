80cfcbcbd2255916369bc34054cec9a3611f9085
Joaoz89 <joao7000paulo@gmail.com>
2022-09-24 00:08:06 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
+		//this way my Program dont know the implementation just interface 
+		//it too a way of us do a dependency injection without explicit the implementation
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 
 	}
